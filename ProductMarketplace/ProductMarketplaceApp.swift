06df56020a9d28825b2be141ede774ea3a907dc5
David Umoh <daveumoh@amazon.com>
2023-11-03 14:55:38 +0100

    Initial Commit

---
+++ ProductMarketplace/ProductMarketplaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductMarketplaceApp.swift
+//  ProductMarketplace
+//
+//  Created by Umoh, David on 2023/11/03.
+//
+
+import SwiftUI
+
+@main
+struct ProductMarketplaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
